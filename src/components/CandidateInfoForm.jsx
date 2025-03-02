bc8fbd13910ebfd4bb5c7fbba932ffb86b04ec36
DriveMeNow <falquerho.marion47@gmail.com>
2025-03-02 03:11:06 +0100

    Ajout de la page Candidate avec formulaire

--- src/components/CandidateInfoForm.jsx
+++
@@ -1,3 +0,0 @@
-<h1 className="text-3xl font-bold underline text-blue-500">
-  Bienvenue sur DriveMeNow !
-</h1>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="bg-blue-500 text-white p-5 text-center text-2xl">
-      Tailwind fonctionne parfaitement !
+    <div className="flex justify-center items-center h-screen bg-blue-100">
+      <h1 className="text-4xl text-blue-600">Bienvenue sur DriveMeNow</h1>
     </div>
   );
 }
