e41ded2713a4457213ff973ddb7d4b3c5da03903
DriveMeNow <falquerho.marion47@gmail.com>
2025-04-02 03:46:04 +0200

    Amélioration directives CSP et nettoyage commentaires sensibles

--- backend/Express.js
+++ backend/Express.js
@@ -1,6 +1,40 @@
-res.cookie('token', jwtToken, {
-    httpOnly: true,
-    secure: true,           // en HTTPS uniquement (essentiel en prod)
-    sameSite: 'Strict',
-    maxAge: 24 * 60 * 60 * 1000, // 24 heures
-  });
+const express = require('express');
+const helmet = require('helmet');
+const crypto = require('crypto');
+
+const app = express();
+
+// Middleware pour générer un nonce unique pour chaque requête
+app.use((req, res, next) => {
+  res.locals.nonce = crypto.randomBytes(16).toString('base64');
+  next();
+});
+
+// Helmet avec CSP stricte + nonce dynamique
+app.use(helmet.hsts({
+  maxAge: 31536000, // 1 an (durée recommandée)
+  includeSubDomains: true, // inclus sous-domaines
+  preload: true // recommandé pour liste préchargée navigateur
+}));
+
+app.use(helmet({
+  contentSecurityPolicy: {
+    useDefaults: true,
+    directives: {
+            "default-src": ["'self'"],
+      "script-src": ["'self'", (req, res) => `'nonce-${res.locals.nonce}'`],
+      "style-src": ["'self'", "https://fonts.googleapis.com", (req, res) => `'nonce-${res.locals.nonce}'`],
+      "font-src": ["'self'", "https://fonts.gstatic.com"],
+      "img-src": ["'self'", "data:", "https:"],
+      "connect-src": ["'self'"],
+      "frame-src": ["'none'"],
+      "frame-ancestors": ["'none'"],
+      "base-uri": ["'self'"],
+      "form-action": ["'self'"],
+      "upgrade-insecure-requests": []
+    }
+  },
+  crossOriginEmbedderPolicy: false
+}));
+
+module.exports = app;
