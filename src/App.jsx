abb09474dab3af91d61bc7183d5ad9e20fa2031b
DriveMeNow <falquerho.marion47@gmail.com>
2025-04-09 02:31:20 +0200

    Corrections pour déploiement Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
-import Dashboard from './pages/Dashboard';
-import Inscription from './pages/Inscription';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/auth" element={<AuthPage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/inscription" element={<Inscription />} />
     </Routes>
   );
 }
