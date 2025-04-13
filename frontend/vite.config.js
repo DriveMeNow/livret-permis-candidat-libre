6f1729c8db1ee33b5b3609885343758e29aeb572
DriveMeNow <falquerho.marion47@gmail.com>
2025-04-13 06:52:26 +0200

    Structure claire et professionnelle avec .gitignore adapté

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // essentiel pour s'assurer que le build va dans frontend/dist
+  },
   server: {
     host: true,  // accepte toutes les connexions externes
     allowedHosts: [
